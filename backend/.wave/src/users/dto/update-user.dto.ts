72ef57001b57f70323b9e9d9bc09057b09f091eb
Bolotinhas <glodsonborges@gmail.com>
2026-05-16 11:00:13 -0300

    feat: add updateuserdto for users

--- backend/.wave/src/users/dto/update-user.dto.ts
+++ backend/.wave/src/users/dto/update-user.dto.ts
@@ -1,4 +1,17 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { IsEnum, IsOptional, IsString, Length } from 'class-validator';
+import { UserRole } from '../entities/user.entity';
+export class UpdateUserDto {
+  @IsOptional() //se o campo vier, valide, senao ignore
+  @IsString()
+  @Length(1, 50)
+  "name"?: string;
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+  @IsOptional()
+  @IsString()
+  @Length(3, 20)
+  "password"?: string;
+
+  @IsOptional()
+  @IsEnum(UserRole)
+  "accountType" ?: UserRole;
+}
