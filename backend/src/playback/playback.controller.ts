4b0375177b22ed618a41fba5dfba13c967b1e75f
Bolotinhas <glodsonborges@gmail.com>
2026-05-17 19:41:17 -0300

    fix: fix routes for playback controller in playback folder

--- backend/src/playback/playback.controller.ts
+++ backend/src/playback/playback.controller.ts
@@ -5,22 +5,7 @@ import { CreatePlaybackDto } from './dto/create-playback.dto';
 @Controller('users/:login/playback')
 export class PlaybackController {
   constructor(private readonly playbackService: PlaybackService) {}
-
-  @Post() //cria playbacks
-  create(@Body() createPlaybackDto: CreatePlaybackDto) {
-    return this.playbackService.create(createPlaybackDto);
-  }
-
-  @Get()
-  findByUser(@Param('login') login:string,){
-    return this.playbackService.findByUser(login);
-  }
-
-  @Get(':id')
-  findOne(@Param('login') login: string, @Param('id', ParseIntPipe) id: number) {
-    return this.playbackService.findOne(login, id,);
-  }
-
+  
   @Get(':type/:id')
   findbytypeandid(@Param('login') login: string, @Param('type') type: 'music' | 'episode', @Param('id', ParseIntPipe) id: number){
     return this.playbackService.findbytype(login, type, id, );
@@ -30,4 +15,20 @@ export class PlaybackController {
   findbytype(@Param('login') login: string, @Param('type') type: 'music' | 'episode'){
     return this.playbackService.findbytype(login, type);
   }
+
+  @Get('id/:id')
+  findOne(@Param('login') login: string, @Param('id', ParseIntPipe) id: number) {
+    return this.playbackService.findOne(login, id,);
+  }
+
+   @Get()
+  findByUser(@Param('login') login:string,){
+    return this.playbackService.findByUser(login);
+  }
+
+  @Post() //cria playbacks
+  create(@Body() createPlaybackDto: CreatePlaybackDto) {
+    return this.playbackService.create(createPlaybackDto);
+  }
+
 }
