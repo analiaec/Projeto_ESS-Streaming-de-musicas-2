783b8b37987ebefb4f923460cc74451a5067007e
Bolotinhas <glodsonborges@gmail.com>
2026-05-17 22:02:13 -0300

    feat: implement create program dto in programs folder

--- backend/src/programas/dto/create-programa.dto.ts
+++ backend/src/programas/dto/create-programa.dto.ts
@@ -1,1 +1,13 @@
-export class CreateProgramaDto {}
+import {IsString, IsNotEmpty, IsArray, ArrayNotEmpty,} from 'class-validator';
+
+export class CreateProgramDto {
+
+  @IsString()
+  @IsNotEmpty()
+  nome: string;
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsString({ each: true })
+  generos: string[];
+}
