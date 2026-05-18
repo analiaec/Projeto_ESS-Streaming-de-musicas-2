9ce32dbc8469315ad44c0990dcf79f62ea877037
Bolotinhas <glodsonborges@gmail.com>
2026-05-18 00:43:50 -0300

    test: implement programs module in programs folder

--- backend/src/programas/programas.module.ts
+++ backend/src/programas/programas.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProgramasService } from './programas.service';
 import { ProgramasController } from './programas.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Programa } from './entities/programa.entity';
+import { Podcast } from '../podcast/entities/podcast.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Programa,Podcast])],
   controllers: [ProgramasController],
   providers: [ProgramasService],
 })
