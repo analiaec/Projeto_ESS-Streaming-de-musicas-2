ed4b86f1200ed3ba6e1d3204166e388f0210c4d8
praxarr <theo.pfarias@gmail.com>
2026-05-18 12:56:17 -0300

    feat: atualiza musicas service

--- backend/src/musicas/musicas.service.ts
+++ backend/src/musicas/musicas.service.ts
@@ -27,9 +27,6 @@ export class MusicasService {
     if(genero) {
         query.andWhere('LOWER(musica.genero) = LOWER(:genero)', {genero : genero});
     }
-    if(genero) {
-        query.andWhere('LOWER(musica.genero) = LOWER(:genero)', {genero : genero});
-    }
     if(artista) {
         query.andWhere('LOWER(artista.nomeArtistico) LIKE LOWER(:artista)', {artista : '%' + artista + '%'});
     }
