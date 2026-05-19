9ffbf562463587f2b35898acbcb954da1f89cc4d
Bolotinhas <glodsonborges@gmail.com>
2026-05-19 13:37:58 -0300

    test: add messages for error situations in createuserdto

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,20 +1,28 @@
-import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, Length, ValidateIf,} from "class-validator";
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, Length, ValidateIf, MinLength, MaxLength} from "class-validator";
 import { UserRole } from "../entities/user.entity";
 
 export class CreateUserDto {
    @IsString()
-   @Length(3,20) //depois implementar os message.
+   @MinLength(3, {message: 'O login deve ter pelo menos 3 caracteres. Construa outro login.',})
+   @MaxLength(20, {message: 'O login não pode ter mais de 20 caracteres.'})
+   @IsNotEmpty({message: 'O campo login não pode estar vazio.'})
    "login": string;
    @IsString()
-   @Length(1, 50)
+   @MinLength(1, {message: 'O nome deve ter pelo menos 1 caractere. Construa outro nome.',})
+   @MaxLength(50, {message: 'O nome não pode ter mais de 50 caracteres.'})
+   @IsNotEmpty({message: 'O campo nome não pode estar vazio.'})
    "name": string;
    @IsString()
-   @Length(3, 20)
+   @MinLength(3, {message: 'A senha deve ter pelo menos 3 caracteres. Construa outra senha.',})
+   @MaxLength(20, {message: 'A senha não pode ter mais de 20 caracteres.'})
+   @IsNotEmpty({message: 'O campo senha não pode estar vazio.'})
    "password": string;
-   @IsEmail()
+   @IsEmail({require_tld: true}, {message: 'Voce deve inserir um e-mail válido para realizar o cadastro. Coloque outro e-mail.'})
+   @IsNotEmpty({message: 'O campo email não pode estar vazio.'})
    "email": string;
-   @IsEnum(UserRole)
-   "role": UserRole;
+   @IsEnum(UserRole, {message: 'O tipo de conta informado deve ser válido.',})
+   @IsNotEmpty({message: 'O campo tipodeconta não pode estar vazio.'})
+   "tipodeconta": UserRole;
    /* @ValidateIf(
     (user) => user.accountType === UserRole.ARTISTA || user.accountType === UserRole.PODCAST,)
     @IsString()
