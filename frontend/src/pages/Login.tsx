dc2326345d4d948e777a48fb3e1557ccc5b842e1
Bolotinhas <glodsonborges@gmail.com>
2026-06-06 14:41:24 -0300

    feat: creating Login.css for visual difference and changing Login.tsx to store user.role and apply Login.css

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,6 +2,7 @@ import { useState }     from 'react';
 import { useNavigate }  from 'react-router-dom';
 import { loginApi }     from '../api';
 import { useAuth }      from '../contexts/AuthContext';
+import './Login.css'
 
 export function Login() {
   const [login,    setLogin]    = useState('');
@@ -13,7 +14,7 @@ export function Login() {
   async function handleLogin() {
     try {
       const dados = await loginApi(login, password);
-      entrar(login, dados.access_token);
+      entrar(login, dados.access_token, dados.role);
       navigate('/');
     } catch (e) {
       setErro('Login ou senha invalidos');
@@ -23,18 +24,24 @@ export function Login() {
   return (
     <div>
       <h1>Login</h1>
+      <div className="botoes-login">
       <input
+        className="campo-login"
         placeholder="Login"
         value={login}
         onChange={e => setLogin(e.target.value)}
       />
       <input
+        className="campo-senha"
         placeholder="Senha"
         type="password"
         value={password}
         onChange={e => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Entrar</button>
+      /></div>
+      <div className="botoes-login">
+      <button className="botao-entrar" onClick={handleLogin}>Entrar</button>
+      <button className="botao-criar" onClick={() => navigate('/auth/register')}>Criar conta</button>
+      </div>
       {erro && <p>{erro}</p>}
     </div>
   );
