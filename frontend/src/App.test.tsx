342caa3caca4ca7f9a9431efa3aa83e5f7bdefbd
Analía <160983109+analiaec@users.noreply.github.com>
2026-06-09 14:51:24 -0300

    Update App.test.tsx

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import NovoAlbum from './pages/NovoAlbum';
+import EditarAlbum from './pages/EditarAlbum';
 
 test('renders learn react link', () => {
   render(<App />);
